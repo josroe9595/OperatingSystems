fbc8e8a7835760b7421e7a42f9543a51c9d89d48
Joseph <josroe9595@gmail.com>
2015-09-21 20:02:05 -0400

    Complete hw2

--- Hw2/hw2.c
+++ Hw2/hw2.c
@@ -1,11 +1,10 @@
-/* fd-write-redirect.c */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <math.h>
 
@@ -20,29 +19,24 @@ int main( int argc, char * argv[])
   int num_proc = (int) tnum_c;
   printf("PARENT: File '%s' contains %d bytes\n",argv[1],(int)buffer.st_size);
   printf("PARENT: ... and will be processed via %d child processes\n",num_proc);
-  pid_t pid;
   char* word_buffer;
-  int check = 0;
-  //pid = fork();
-
-  pid = fork();
-  int orig = getpid();
-  while (check < num_proc){
-    check+=1;
-    pid = fork();
-    check+=1;
-    if (pid > 0){
-    	word_buffer = malloc(sizeof(char)*(num_by));
-      read(fil,word_buffer,sizeof(char)*num_by);
-      printf("CHILD %d CHUNK: %s\n",getpid(),word_buffer);
-      fflush(stdout);
-      int x = getpid();
-      if (x - orig < num_proc) break;
-      //if (strlen(word_buffer) < 1) {printf("DOESTHIS\n\n\n\n");fflush(stdout);}
-
-      //check += 1;
-    } 
-    wait();
-    
+  pid_t pid; ;
+  int x;
+  int status;
+  for (x = 0; x < num_proc; x++){
+    pid = fork();  
+    if (pid == 0){ 
+      break;
+    }
+    wait(&status);
   }
+  if (pid == 0) {
+    word_buffer = malloc(sizeof(char)*(num_by) + 1);
+    read(fil,word_buffer,sizeof(char)*num_by);
+    printf("CHILD %d CHUNK: %s\n",getpid(),word_buffer);
+    fflush(stdout);
+    wait(&status);
+    exit(EXIT_SUCCESS);
+  }
+  return EXIT_SUCCESS;
 }
