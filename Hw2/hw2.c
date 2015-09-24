dd2e05b854037fc16a82f752691a65b330acfc05
Joseph <josroe9595@gmail.com>
2015-09-24 17:14:01 -0400

    Added Error Checking

--- Hw2/hw2.c
+++ Hw2/hw2.c
@@ -10,6 +10,12 @@
 
 int main( int argc, char * argv[])
 {
+  if (argc != 3){
+    perror("ERROR: Invalid arguments\n");
+    perror("USAGE: ./a.out <input-file> <chunk-size>\n");
+    return EXIT_FAILURE;
+
+  }
   int fil = open(argv[1],'r');
   int num_by = atoi(argv[2]);
   struct stat buffer;
@@ -20,7 +26,7 @@ int main( int argc, char * argv[])
   printf("PARENT: File '%s' contains %d bytes\n",argv[1],(int)buffer.st_size);
   printf("PARENT: ... and will be processed via %d child processes\n",num_proc);
   char* word_buffer;
-  pid_t pid; ;
+  pid_t pid;
   int x;
   int status;
   for (x = 0; x < num_proc; x++){
@@ -28,14 +34,21 @@ int main( int argc, char * argv[])
     if (pid == 0){ 
       break;
     }
-    wait(&status);
+    pid_t child_pid = wait(&status);
+    if (WIFSIGNALED(status))  
+    {
+      printf( "PARENT: child %d terminated abnormally\n",(int)child_pid );
+    }
+    if (WEXITSTATUS(status) != 0){
+      printf("PARENT: child %d terminated with nonzero exit status %d",(int)child_pid,status);
+    }
   }
   if (pid == 0) {
     word_buffer = malloc(sizeof(char)*(num_by) + 1);
     read(fil,word_buffer,sizeof(char)*num_by);
     printf("CHILD %d CHUNK: %s\n",getpid(),word_buffer);
     fflush(stdout);
-    wait(&status);
+    //wait(&status);
     exit(EXIT_SUCCESS);
   }
   return EXIT_SUCCESS;
